e3693ff273497759dcd7bf9f55f180b7732ae3f5
Maria <Semenova.semyono@yandex.ru>
2025-08-13 17:08:18 +0300

    first version

---
+++ DB.sql
@@ -0,0 +1,15 @@
+CREATE TABLE password_manager (
+    id SERIAL PRIMARY KEY,
+    service_name TEXT NOT NULL,
+    login TEXT NOT NULL,
+    password TEXT NOT NULL,
+    notes TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE UNIQUE INDEX uniq_service_login
+ON password_manager (service_name, login);
+
+SELECT * FROM password_manager;
+
